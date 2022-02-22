0c75cda856889403c48eda3b1e71d59476120069
dmargan <davor@mar-com.hr>
2022-02-22 22:18:40 +0100

    Davor push test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-center">Recipe Library</h1>
+      <p>Are you hungry?</p>
     </div>
   );
 }

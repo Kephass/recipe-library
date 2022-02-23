eed167ffb4158ee39f5967d1f5587a491ba012de
dmargan <davor@mar-com.hr>
2022-02-23 10:38:07 +0100

    created All Recipes Page Component

--- src/pages/AllRecipes.js
+++ src/pages/AllRecipes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function AllRecipesPage() {
-  return <h1 className="text-3xl font-bold underline">AllRecipesPage</h1>;
+  return <h1 className="text-3xl font-bold underline">All Recipes Page</h1>;
 }
 
 export default AllRecipesPage;

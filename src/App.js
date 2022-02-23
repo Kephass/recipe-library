d9cfcab1d2a3d1a8a270d767807973ab5f277259
Felix Oduro Fosu <45005547+Kephass@users.noreply.github.com>
2022-02-23 17:07:27 +0100

    Reorganized and tested routing setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Landing from "./Pages/Landing";
+import Recipes from "./Pages/Recipes";
+import ErrorPage from "./Pages/ErrorPage";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center">Recipe Library</h1>
-      <p className="mb-4">Are you hungry?</p>
-
-      <Link
-        to="/test"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Click Me!
-      </Link>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/test" element={<h1>I'am test page!</h1>} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+        <nav className="buttons mx-auto text-center">
+          <Link
+            to="/test"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Test
+          </Link>
+          <Link
+            className="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            to="/recipes"
+          >
+            Recipes
+          </Link>
+        </nav>
+      </Router>
 
       {/* TODO: Create route to "/test" using React Router v6, make it render a simple element to screen */}
     </div>
---
+++ src/Pages/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-center mb-5">This is the landing page</h1>
+    </div>
+  );
+}
+
+export default Landing;

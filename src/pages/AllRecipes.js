67edd2bab2405391d1d439914a16ae63fd4682d8
Felix Oduro Fosu <45005547+Kephass@users.noreply.github.com>
2022-02-23 12:19:20 +0100

    Revert "Davor"

--- src/pages/AllRecipes.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function AllRecipesPage() {
-  return <h1 className="text-3xl font-bold underline">All Recipes Page</h1>;
-}
-
-export default AllRecipesPage;
--- src/pages/Home.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function HomePage() {
-  return <h1 className="text-3xl font-bold underline">Recipe Library</h1>;
-}
-
-export default HomePage;

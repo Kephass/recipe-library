2037a9b8e4bdfe09523bfeee158e41ab046cba12
Felix Oduro Fosu <45005547+Kephass@users.noreply.github.com>
2022-02-25 00:32:18 +0100

    built the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Box } from '@chakra-ui/react';
 import Navigation from './Components/Layout/Navigation';
 import Landing from './Pages/Landing';
+import Search from './Pages/Search';
 import Recipes from './Pages/Recipes';
 import ErrorPage from './Pages/ErrorPage';
 
 function App() {
 	return (
-		<div>
+		<Box maxW='1200px' bg='#252836' mx='auto'>
 			<Router>
 				<Routes>
 					<Route path='/' element={<Landing />} />
+					<Route path='/search' element={<Search />} />
 					<Route path='/recipes' element={<Recipes />} />
 					<Route path='/test' element={<h1>I'am test page!</h1>} />
 					<Route path='*' element={<ErrorPage />} />
@@ -18,7 +21,7 @@ function App() {
 			</Router>
 
 			{/* TODO: Create route to "/test" using React Router v6, make it render a simple element to screen */}
-		</div>
+		</Box>
 	);
 }
 

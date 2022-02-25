c323adf487f2673c5aafb8f194dfbaa5ce702661
Matthew McGrath <rubywep@hotmail.com>
2022-02-24 20:28:14 -0800

    Refactor the way Page components are imported.
    This practice of including all Components related to a single "Feature" (in this case, a "Page") is pretty standard. We use an index file to import all the Page components, and then export them as a single object. This then allows us, for example, in App.js to import all Page components with a single import statement.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
 import Navigation from './Components/Layout/Navigation';
-import Landing from './Pages/Landing';
-import Search from './Pages/Search';
-import Recipes from './Pages/Recipes';
-import ErrorPage from './Pages/ErrorPage';
+import { ErrorPage, Landing, Recipe, Recipes, Search, Test } from './Pages';
 
 function App() {
-	return (
-		<Box maxW='1200px' bg='#252836' mx='auto'>
-			<Router>
-				<Routes>
-					<Route path='/' element={<Landing />} />
-					<Route path='/search' element={<Search />} />
-					<Route path='/recipes' element={<Recipes />} />
-					<Route path='/test' element={<h1>I'am test page!</h1>} />
-					<Route path='*' element={<ErrorPage />} />
-				</Routes>
-				<Navigation />
-			</Router>
-
-			{/* TODO: Create route to "/test" using React Router v6, make it render a simple element to screen */}
-		</Box>
-	);
+  return (
+    <Box maxW='1200px' bg='#252836' mx='auto'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/recipes' element={<Recipes />} />
+          <Route path='/recipes/:recipeId' element={<Recipe />} />
+          <Route path='/test' element={<Test />} />
+          <Route path='*' element={<ErrorPage />} />
+        </Routes>
+        <Navigation />
+      </Router>
+    </Box>
+  );
 }
 
 export default App;

431335f74634ed82c3055baae15dfba1699ecf79
dmargan <davor@mar-com.hr>
2022-02-25 17:26:04 +0100

    added categories page

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,7 +2,8 @@ import ErrorPage from './ErrorPage';
 import Landing from './Landing';
 import Recipe from './Recipe';
 import Recipes from './Recipes';
+import Categories from './Categories';
 import Search from './Search';
 import Test from './Test';
 
-export { ErrorPage, Landing, Recipe, Recipes, Search, Test };
+export { ErrorPage, Landing, Recipe, Recipes, Search, Categories, Test };

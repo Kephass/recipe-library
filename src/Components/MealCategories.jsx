a5d29818d770e79159ba124303f206c5720bad5f
Matthew McGrath <rubywep@hotmail.com>
2022-02-25 20:17:00 -0800

    More cateogries. Can fetch results by category.
    I added the other categories that the Spoonacular API allows to be used for querying. Some of them I did not find a suitable emoji for, yet. Clicking the 'See all' button increases the number of categories shown from the initial 5 to the full size of 14. The initial category is chosen based off the user's time (Breakfast, Main Course, or Dessert). Clicking a category retrieves the first 10 results. You can then click on one of the results and be taken to the detailed view of that recipe. A lot of API and Styling work to do! This was just to get the ball rolling.

--- src/Components/MealCategories.jsx
+++ src/Components/MealCategories.jsx
@@ -1,7 +1,17 @@
-import React, { Fragment, useState } from 'react';
-import { Heading, Flex, Link, Spacer, Stack } from '@chakra-ui/react';
+import React, { Fragment, useEffect, useState } from 'react';
+import {
+  Heading,
+  Image,
+  Flex,
+  Link,
+  Spacer,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 import { Link as ReactLink } from 'react-router-dom';
 import CategoryButton from './CategoryButton';
+import { getRecipe } from '../api/recipeSearch';
+import * as mealHelper from '../helpers/meal';
 
 const CATEGORIES = [
   { id: 1, text: 'breakfast', icon: '🍰' },
@@ -9,23 +19,83 @@ const CATEGORIES = [
   { id: 3, text: 'dessert', icon: '🍰' },
   { id: 4, text: 'salad', icon: '🥬' },
   { id: 5, text: 'soup', icon: '🥣' },
+  { id: 6, text: 'side dish', icon: '🤷‍♂️' },
+  { id: 7, text: 'appetizer', icon: '🤷‍♂️' },
+  { id: 8, text: 'beverage', icon: '🍺' },
+  { id: 9, text: 'snack', icon: '🤷‍♂️' },
+  { id: 10, text: 'drink', icon: '🍹' },
+  { id: 11, text: 'fingerfood', icon: '🤷‍♂️' },
+  { id: 12, text: 'bread', icon: '🍞' },
+  { id: 13, text: 'sauce', icon: '🤷‍♂️' },
+  { id: 14, text: 'marinade', icon: '🤷‍♂️' },
 ];
 
-const DEFAULT_CATEGORY = CATEGORIES[0].id;
+const DEFAULT_CATEGORY = mealHelper.recommendedMealCategory();
 
 function MealCategories() {
   const [selectedCategory, setSelectedCategory] = useState(DEFAULT_CATEGORY);
+  const [categoryCount, setCategoryCount] = useState(5);
+  const [suggestedRecipes, setSuggestedRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchMealsFromCategory = async () => {
+      const params = {};
+      params.type = CATEGORIES[selectedCategory - 1].text.replace(' ', '');
+      const fetchedRecipes = await getRecipe(params);
+      setSuggestedRecipes(fetchedRecipes.results);
+      console.log('Category changed!', params.type);
+    };
+
+    fetchMealsFromCategory();
+  }, [selectedCategory]);
+
+  const renderedSuggestionList = () => {
+    return suggestedRecipes.map((recipe) => {
+      return (
+        <ReactLink to={`/recipes/${recipe.id}`}>
+          <Flex
+            key={recipe.id}
+            spacing={4}
+            direction='row'
+            align='center'
+            flexWrap='wrap'
+            gap={2}
+            my={4}
+            backgroundColor='secondary'
+            borderRadius='3xl'
+            p={2}
+            boxShadow='xl'
+          >
+            <Image
+              src={recipe.image}
+              alt={recipe.title}
+              borderRadius='2xl'
+              height='100px'
+            />
+            <Text>{recipe.title}</Text>
+          </Flex>
+        </ReactLink>
+      );
+    });
+  };
   return (
     <Fragment>
       <Flex align='end' mt='10' mb='5'>
         <Heading as='h3'>Meal Category</Heading>
         <Spacer />
-        <Link as={ReactLink} to='/categories' _hover={{ color: 'testYellow' }}>
+        <Link
+          as={ReactLink}
+          to='#'
+          _hover={{ color: 'testYellow' }}
+          onClick={() => {
+            setCategoryCount(CATEGORIES.length);
+          }}
+        >
           See all
         </Link>
       </Flex>
-      <Stack spacing={4} direction='row' align='center'>
-        {CATEGORIES.map((category) => (
+      <Stack spacing={4} direction='row' align='center' flexWrap='wrap' gap={2}>
+        {CATEGORIES.slice(0, categoryCount).map((category) => (
           <CategoryButton
             key={category.id}
             onClickHandler={setSelectedCategory}
@@ -33,6 +103,9 @@ function MealCategories() {
             category={category}
           />
         ))}
+      </Stack>
+      <Stack spacing={4} direction='row' align='center' flexWrap='wrap' gap={2}>
+        {renderedSuggestionList()}
       </Stack>
     </Fragment>
   );

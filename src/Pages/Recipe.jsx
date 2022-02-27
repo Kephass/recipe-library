bf0c2596261b205641f40f52924e2b8ed07fc93c
Matthew McGrath <rubywep@hotmail.com>
2022-02-26 22:16:09 -0800

    Add very minor styling
    Began revamping the styling of the Recipe page to look more like the Dribbble inspiration. Couldn't take it too much further, as it looks like we ran out of API calls for the day. I'll have to create some more dummy meal data properties (description, ingredients, etc) so I have some data to look at when working on this screen!

--- src/Pages/Recipe.jsx
+++ src/Pages/Recipe.jsx
@@ -1,38 +1,70 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Heading, Image, Link, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  Link,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 import { getRecipeById } from '../api/recipeSearch';
 
 function Recipe() {
-	const { recipeId } = useParams();
-	const [recipe, setRecipe] = useState({});
+  const { recipeId } = useParams();
+  const [recipe, setRecipe] = useState({});
 
-	const getRecipe = async () => {
-		if (recipeId == 1) return;
+  const getRecipe = async () => {
+    if (recipeId == 1) return;
 
-		const fetchedRecipe = await getRecipeById(recipeId);
-		setRecipe(fetchedRecipe);
-	};
+    const fetchedRecipe = await getRecipeById(recipeId);
+    setRecipe(fetchedRecipe);
+  };
 
-	useEffect(() => {
-		getRecipe();
-	}, []);
+  useEffect(() => {
+    getRecipe();
+  }, []);
 
-	if (!recipe) return <div>Fetching recipe...</div>;
-	console.log(recipe);
+  if (!recipe) return <div>Fetching recipe...</div>;
+  console.log(recipe);
 
-	return (
-		<Container maxW='container.xl' minH='93vh'>
-			<Heading as='h1'>{recipe.title}</Heading>
-			<Text>{recipe.title}</Text>
-			<Text>Time: {recipe.readyInMinutes}</Text>
-			<Image src={recipe.image} />
-			<Text>{recipe.summary}</Text>
-			<Link href={recipe.sourceUrl} isExternal>
-				Read Original Article
-			</Link>
-		</Container>
-	);
+  return (
+    <Container
+      maxW='container.xl'
+      minH='93vh'
+      py={6}
+      centerContent
+      bgColor='primary'
+    >
+      <Image src={recipe.image} />
+      <Heading as='h1'>{recipe.title}</Heading>
+      <HStack>
+        <Box>
+          <Text>**</Text>
+          <Text>Time: {recipe.readyInMinutes}</Text>
+          <Text>Cooking</Text>
+        </Box>
+        <Box>
+          <Text>**</Text>
+          <Text>4.08</Text>
+          <Text>Rating</Text>
+        </Box>
+        <Box>
+          <Text>**</Text>
+          <Text>Easy level</Text>
+          <Text>Recipes</Text>
+        </Box>
+      </HStack>
+
+      <Text>{recipe.summary}</Text>
+      <Link href={recipe.sourceUrl} isExternal>
+        Read Original Article
+      </Link>
+    </Container>
+  );
 }
 
 export default Recipe;

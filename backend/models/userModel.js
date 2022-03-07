b9d02431851d7c7f7998c840335cc4393e03dce0
Matthew McGrath <rubywep@hotmail.com>
2022-03-07 13:03:08 -0800

    Quick push of all my current changes

---
+++ backend/models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+// Defines the shape of a User model
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please enter a user name'],
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    // TODO: This will be an array of ObjectID references (they will refer to Recipe models we will soon define)
+    favoriteMeals: {
+      type: Array,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('User', userSchema);

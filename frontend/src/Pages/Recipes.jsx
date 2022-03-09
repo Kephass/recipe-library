2c24f9e17ca7fa6a5b4e86ee5325b9ecb3099570
Matthew McGrath <rubywep@hotmail.com>
2022-03-08 23:35:11 -0800

    Add fetching of recipes from our database
    The Recipes page now grabs all the recipes (only 4 so far) from our mongo database and renders them in a very basic way.

--- frontend/src/Pages/Recipes.jsx
+++ frontend/src/Pages/Recipes.jsx
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { recipeService } from '../api/recipes.service';
+import { useFetch } from '../utils/hooks/useFetch';
 
 function ScreenRecipes() {
-  let navigate = useNavigate();
+  const navigate = useNavigate();
+
+  const [data, loading, error] = useFetch(recipeService.getFromServer());
+
+  useEffect(() => {}, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching from MongoDB</div>;
+  }
+
+  if (!data) {
+    return <div>No fetched data to show</div>;
+  }
+
+  const renderedRecipeResults = (
+    <ul>
+      {data.map((recipe) => {
+        return (
+          <li key={recipe._id}>
+            <h2>{recipe.title}</h2>
+            <img src={recipe.image} alt={recipe.title} />
+            <div>Prep Time: {recipe.timeToPrepare} mins</div>
+            <div>Difficult: {recipe.difficulty}</div>
+            <div>Rating: {recipe.rating}</div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+
   return (
     <div>
-      <div className='recipes mb-9  text-center'>
+      <div className="recipes mb-9  text-center">
         <h1>Here are some recipes</h1>
+        <p>They are being fetched from MongoDB on localhost:5000!</p>
+        {data && renderedRecipeResults}
         <button
-          className='bg-green-500 rounded p-2'
+          className="bg-green-500 rounded p-2"
           onClick={() => navigate('/')}
         >
-          back home
+          Back Home
         </button>
       </div>
     </div>

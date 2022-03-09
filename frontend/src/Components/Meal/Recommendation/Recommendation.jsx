71835731181664d8238641730eb5222c97c58ec3
Matthew McGrath <rubywep@hotmail.com>
2022-03-08 23:41:36 -0800

    Fix bug with meal not actually being fetched
    The Recommendation is now fetched propertly from Spoonacular, using the appropriate mealType (calculated by the user's current time of day)

--- frontend/src/Components/Meal/Recommendation/Recommendation.jsx
+++ frontend/src/Components/Meal/Recommendation/Recommendation.jsx
@@ -5,25 +5,21 @@ import { ClockCircleOutlined, FireOutlined } from '@ant-design/icons';
 import currentMealTime from '../../../utils/currentMealTime';
 import useFetch from '../../../utils/hooks/useFetch';
 import { recipeService } from '../../../api/recipes.service';
-import { meal as DUMMY_RECIPE } from '../../../utils/mockData';
 
 export function MealRecommendation() {
+  const { mealType, recommendationMessage } = currentMealTime();
+
   /* Implementation of useFetch hook. Still grasping how to implent a cleaner way to pass
   helper fn in this fetch custom hook and how to pass helpers that accept arguments like ex: recipe_id. 
   I still don't if this is good approach or not. Please tell me what you think? */
-  // const [data, loading, error] = useFetch(recipeService.getById(33));
   const [data, loading, error] = useFetch(
-    recipeService.getByParams({ type: 'breakfast' })
+    recipeService.getRecipesByParams({ type: mealType, number: 1 })
   );
-  const { recommendationMessage } = currentMealTime();
-  // If there is no recipe from API load local dummy recipe
-  // const recipe = data?.recipes[0] || DUMMY_RECIPE;
-  const recipe = data || DUMMY_RECIPE;
 
   console.log('DATA', data);
 
   if (error) {
-    //This need to go in some global Error handler.
+    // This need to go in some global Error handler.
     console.log(error);
     return <p>Something went wrong!</p>;
   }
@@ -31,41 +27,41 @@ export function MealRecommendation() {
   //This should be returning UI component with some spiner.
   if (loading) return <p>Loading...</p>;
   return (
-    <Container maxW='container.xl' centerContent>
-      <Link to={`/recipes/${recipe.id}`}>
+    <Container maxW="container.xl" centerContent>
+      <Link to={`/recipes/${data.results[0].id}`}>
         <Flex
-          background={`linear-gradient(to right, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.35)), url('${recipe.image}')`}
+          background={`linear-gradient(to right, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.35)), url('${data.results[0].image}')`}
           p={8}
           w={500}
           h={200}
-          backgroundSize='cover'
+          backgroundSize="cover"
           borderRadius={20}
-          flexDir='column'
+          flexDir="column"
           lineHeight={1}
-          color='white'
-          justify='space-around'
+          color="white"
+          justify="space-around"
         >
-          <Text color='textFaint' fontWeight='bold'>
+          <Text color="textFaint" fontWeight="bold">
             {recommendationMessage}
           </Text>
-          <Text fontSize={24} color='testYellow'>
-            {recipe.title}
+          <Text fontSize={24} color="testYellow">
+            {data.results[0].title}
           </Text>
           <Flex gap={4}>
-            <Flex align='center' justify='center' gap={2}>
-              <Box bg='testYellow' p={2} borderRadius='md'>
+            <Flex align="center" justify="center" gap={2}>
+              <Box bg="testYellow" p={2} borderRadius="md">
                 <ClockCircleOutlined style={{ color: 'black' }} />
               </Box>
-              <Text fontSize={24} color='testYellow'>
-                {recipe.timeToPrepare}
+              <Text fontSize={24} color="testYellow">
+                {data.results[0].timeToPrepare}
               </Text>
             </Flex>
-            <Flex align='center' justify='center' gap={2}>
-              <Box bg='testYellow' p={2} borderRadius='md'>
+            <Flex align="center" justify="center" gap={2}>
+              <Box bg="testYellow" p={2} borderRadius="md">
                 <FireOutlined style={{ color: 'black' }} />
               </Box>
-              <Text fontSize={24} color='testYellow'>
-                {recipe.difficulty}
+              <Text fontSize={24} color="testYellow">
+                {data.results[0].difficulty}
               </Text>
             </Flex>
           </Flex>

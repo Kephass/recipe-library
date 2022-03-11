9118f8ea7b0b0d523ed8dc56945b3b34854b10fc
Matthew McGrath <rubywep@hotmail.com>
2022-03-10 16:21:53 -0800

    Begin logic for Search Page
    By visiting '/search/<searchTerm>', the app will appropriately fetch 5 recipes that have that search term as a match. For now, there is no input for the user to type their own search -- we can only enter the search directly into the /search/ URL.
    
    The styling of the returned list of recipes is very basic and unfinished. But you can click on a result and be taken to its detailed recipe page!

--- frontend/src/Pages/Search.jsx
+++ frontend/src/Pages/Search.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import useFetch from '../utils/hooks/useFetch';
+import { recipeService } from '../api/recipes.service';
 
 function ScreenSearch() {
-  return <div>Search Screen Placeholder</div>;
+  const { query } = useParams();
+  // TODO: The URL returned by getRecipesByParams contains the data in a "results" object. How do we access that with useFetch?
+  // const [searchedRcipes, isLoading, hasError] = useFetch(
+  //   recipeService.getRecipesByParams({ query: query, number: 10 })
+  // );
+  const [searchedRecipes, setSearchedRecipes] = useState([]);
+
+  const fetchRecipeByQuery = async (recipeName) => {
+    const fetchedData = await axios.get(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_SPOON_API}&query=${query}&number=5`
+    );
+
+    const fetchedRecipes = fetchedData.data;
+    setSearchedRecipes(fetchedRecipes.results);
+  };
+
+  // Re-fetch every time the query param changes
+  useEffect(() => {
+    fetchRecipeByQuery(query);
+  }, [query]);
+
+  if (!searchedRecipes) return <div>Fetching {query} recipes...</div>;
+
+  return (
+    <div>
+      <h1>Search Recipe Screen</h1>
+      {searchedRecipes.map((recipe) => {
+        return (
+          <Link id={recipe.id} to={`/recipes/${recipe.id}`}>
+            <h2>{recipe.title}</h2>
+            <img src={recipe.image} alt={recipe.title} />
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
 
 export default ScreenSearch;
